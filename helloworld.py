55b1d5dca42f26d2ec6277b46f1b059c7b2cffb4
pokemonjiwoo <ziwo0o@naver.com>
2024-06-26 17:15:46 +0900

    사칙연산 변동 사항
    사칙연산 변동 사항

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+print("hello world")
+
+name = input("이름을 입력하세요 :")
+age = input("나이를 입력하세요 :")
+
+
+# print("안녕하세요, name님")
+# print("안녕하세요." + name + "님. 반갑습니다.". 입력하신 나이는 " + age + "입니다.")
+# print("{name}은 {age}살 입니다.")
+
